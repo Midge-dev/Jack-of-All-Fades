e9636fde86e6554669015f880a7d70f2c9da923c
Sean M <smidgley792@gmail.com>
2021-10-16 13:51:27 -0700

    contact links

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+// if( (navigator.platform.indexOf("iPhone") != -1) 
+//         || (navigator.platform.indexOf("iPod") != -1)
+//         || (navigator.platform.indexOf("iPad") != -1))
+//          window.open("maps://www.google.com/maps/dir/?api=1&travelmode=driving&layer=traffic&destination=[YOUR_LAT],[YOUR_LNG]");
+//     else
+//          window.open("https://www.google.com/maps/dir/?api=1&travelmode=driving&layer=traffic&destination=[YOUR_LAT],[YOUR_LNG]");
+// }
